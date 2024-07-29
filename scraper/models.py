e11625afa2dc57eed627579652c4fd9e9465fb0b
saeideh-eslamian <s.eslamian.70@gmail.com>
2024-07-29 18:28:22 +0200

    feat: pass test test_channel_creation

--- scraper/models.py
+++ scraper/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 class Channel(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    # The max length of youtube channel is 24
+    youtube_id = models.CharField(max_length=24, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_channel_youtube_url(self):
+        return f"https://www.youtube.com/@{self.channel_youtube_id}"
